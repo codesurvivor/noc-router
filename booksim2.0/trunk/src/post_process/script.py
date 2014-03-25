6faf82ede5d8d42b188a1de959f47b08e64fbcc0
Chao Chen <chao.chen@polymtl.ca>
2014-03-25 15:13:21 -0400

    script.py: NoP added to do: my_nop_mesh()

--- booksim2.0/trunk/src/post_process/script.py
+++ booksim2.0/trunk/src/post_process/script.py
@@ -29,16 +29,28 @@ for n in range(len(dataRate)):
 	allData = map(float, open('dump.txt').readlines())
 	dataOddEven.append(allData[0])
 
-# DyXY routing
-dataDyXY = []
-routing = "dy_xy"
+## DyXY routing
+#dataDyXY = []
+#routing = "dy_xy"
+#print routing
+#for n in range(len(dataRate)):
+#	print "Inj. Rate:", dataRate[n]
+#	with open(os.devnull, "w") as fnull:
+#		call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), "routing_function = " + routing], stdout=fnull)
+#	allData = map(float, open('dump.txt').readlines())
+#	dataDyXY.append(allData[0])
+
+# NoP routing
+dataNoP = []
+routing = "my_nop"
 print routing
 for n in range(len(dataRate)):
 	print "Inj. Rate:", dataRate[n]
 	with open(os.devnull, "w") as fnull:
 		call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), "routing_function = " + routing], stdout=fnull)
 	allData = map(float, open('dump.txt').readlines())
-	dataDyXY.append(allData[0])
+	dataNoP.append(allData[0])
+
 
 
 # print	
@@ -51,9 +63,14 @@ print "======Odd Even packet latencies======"
 for n in range(len(dataRate)):
 	print dataOddEven[n]
 
-print "======DyXY packet latencies======"
+#print "======DyXY packet latencies======"
+#for n in range(len(dataRate)):
+#	print dataDyXY[n]
+
+print "======NoP packet latencies======"
 for n in range(len(dataRate)):
-	print dataDyXY[n]
+	print dataNoP[n]
+
 
 
 # plot figure
@@ -61,7 +78,8 @@ fig = plt.figure()
 
 plt.plot(dataRate, dataXY, label='XY')
 plt.plot(dataRate, dataOddEven, label='Odd Even')
-plt.plot(dataRate, dataDyXY, label='DyXY')
+#plt.plot(dataRate, dataDyXY, label='DyXY')
+plt.plot(dataRate, dataNoP, label='NoP')
 
 plt.legend(loc='upper left')
 plt.xlabel('Injection Rate (Packets/Cycle)')
