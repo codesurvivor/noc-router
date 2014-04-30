320d04c47ba7d57864a370fea9b3272f3a06c724
Chao Chen <chao.chen@polymtl.ca>
2014-04-30 13:05:02 -0400

    post_process/script.py: function added

--- booksim2.0/trunk/src/post_process/script.py
+++ booksim2.0/trunk/src/post_process/script.py
@@ -2,146 +2,70 @@ from subprocess import call
 import matplotlib.pyplot as plt
 import os
 
+def start_sim(routing, dataRate):
+	result = [0]*len(dataRate)
+	print routing
+	for n in range(len(dataRate)):
+		print "Inj. Rate:", dataRate[n]
+		with open(os.devnull, "w") as fnull:
+			call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), 
+	          "routing_function = " + routing], stdout=fnull)
+		allData = map(float, open('dump.txt').readlines())
+		os.remove('dump.txt')
+		result[n] = allData[0]
+
+	return result
+
+def print_msg(data, routing):
+	print  "======", routing, "latencies======"
+	for n in range(len(data)):
+		print data[n]
+
 # injection rate
 dataRate = [];
-for n in range(1, 5):
+for n in range(1, 9):
 	dataRate.append(n*0.001)
 
+# start simulation
 # XY routing
-dataXY = []
-routing = "my_xy"
-print routing
-for n in range(len(dataRate)):
-	print "Inj. Rate:", dataRate[n]
-	with open(os.devnull, "w") as fnull:
-		call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), 
-          "routing_function = " + routing], stdout=fnull)
-	allData = map(float, open('dump.txt').readlines())
-	os.remove('dump.txt')
-	dataXY.append(allData[0])
-
-## Odd Even routing
-#dataOddEven = []
-#routing = "odd_even"
-#print routing
-#for n in range(len(dataRate)):
-#	print "Inj. Rate:", dataRate[n]
-#	with open(os.devnull, "w") as fnull:
-#		call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), "routing_function = " + routing], stdout=fnull)
-#	allData = map(float, open('dump.txt').readlines())
-#	os.remove('dump.txt')
-#	dataOddEven.append(allData[0])
-
-# DyXY routing
-dataDyXY = []
-routing = "dy_xy"
-print routing
-for n in range(len(dataRate)):
-	print "Inj. Rate:", dataRate[n]
-	with open(os.devnull, "w") as fnull:
-		call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), "routing_function = " + routing], stdout=fnull)
-	allData = map(float, open('dump.txt').readlines())
-	os.remove('dump.txt')
-	dataDyXY.append(allData[0])
-
-## NoP routing
-#dataNoP = []
-#routing = "my_nop"
-#print routing
-#for n in range(len(dataRate)):
-#	print "Inj. Rate:", dataRate[n]
-#	with open(os.devnull, "w") as fnull:
-#		call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), "routing_function = " + routing], stdout=fnull)
-#	allData = map(float, open('dump.txt').readlines())
-#	os.remove('dump.txt')
-#	dataNoP.append(allData[0])
+dataXY = start_sim('my_xy', dataRate)
 
 # West first routing
-dataWest = []
-routing = "west_first"
-print routing
-for n in range(len(dataRate)):
-	print "Inj. Rate:", dataRate[n]
-	with open(os.devnull, "w") as fnull:
-		call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), "routing_function = " + routing], stdout=fnull)
-	allData = map(float, open('dump.txt').readlines())
-	os.remove('dump.txt')
-	dataWest.append(allData[0])
-
-
-# North last routing
-dataNorth = []
-routing = "north_last"
-print routing
-for n in range(len(dataRate)):
-	print "Inj. Rate:", dataRate[n]
-	with open(os.devnull, "w") as fnull:
-		call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), "routing_function = " + routing], stdout=fnull)
-	allData = map(float, open('dump.txt').readlines())
-	os.remove('dump.txt')
-	dataNorth.append(allData[0])
-
+dataWest = start_sim('west_first', dataRate)
 
 # Negative first routing
-dataNegative = []
-routing = "negative_first"
-print routing
-for n in range(len(dataRate)):
-	print "Inj. Rate:", dataRate[n]
-	with open(os.devnull, "w") as fnull:
-		call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), "routing_function = " + routing], stdout=fnull)
-	allData = map(float, open('dump.txt').readlines())
-	os.remove('dump.txt')
-	dataNegative.append(allData[0])
+dataNegative = start_sim('negative_first', dataRate)
+
+## Odd Even routing
+dataOddEven = start_sim('odd_even', dataRate)
+
+# NoP routing
+dataNoP = start_sim('my_nop', dataRate)
 
 
 
 # print	
-print "======XY packet latencies======"
-for n in range(len(dataRate)):
-	print dataXY[n]
+print_msg(dataXY, "XY")
+print_msg(dataWest, "West first")
+print_msg(dataNegative, "Negative first")
+print_msg(dataOddEven, "Odd Even")
+print_msg(dataNoP, "NoP")
 
-
-#print "======Odd Even packet latencies======"
-#for n in range(len(dataRate)):
-#	print dataOddEven[n]
-
-print "======DyXY packet latencies======"
-for n in range(len(dataRate)):
-	print dataDyXY[n]
-
-#print "======NoP packet latencies======"
-#for n in range(len(dataRate)):
-#	print dataNoP[n]
-
-print "======West first packet latencies======"
-for n in range(len(dataRate)):
-	print dataWest[n]
-
-print "======North last packet latencies======"
-for n in range(len(dataRate)):
-	print dataNorth[n]
-
-print "======Negative first packet latencies======"
-for n in range(len(dataRate)):
-	print dataNegative[n]
-
-
-
-exit();
+#exit();
 
 # plot figure
 fig = plt.figure()
 
 plt.plot(dataRate, dataXY, label='XY')
-#plt.plot(dataRate, dataOddEven, label='Odd Even')
-plt.plot(dataRate, dataDyXY, label='DyXY')
-#plt.plot(dataRate, dataNoP, label='NoP')
 plt.plot(dataRate, dataWest, label='West first')
+plt.plot(dataRate, dataNegative, label='Negative first')
+plt.plot(dataRate, dataOddEven, label='Odd Even')
+plt.plot(dataRate, dataNoP, label='NoP')
 
 plt.legend(loc='upper left')
 plt.xlabel('Injection Rate (Packets/Cycle)')
 plt.ylabel('Average packet latencies (Cycles)')
+plt.grid()
 
 plt.savefig("latency.eps")
 
