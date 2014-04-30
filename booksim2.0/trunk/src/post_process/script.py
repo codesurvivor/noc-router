ae54be6a00d5e0600414920c448a7b1dfa9c2fc7
Chao Chen <chao.chen@polymtl.ca>
2014-04-30 13:19:45 -0400

    post_process/script.py: dataRateXY ... added

--- booksim2.0/trunk/src/post_process/script.py
+++ booksim2.0/trunk/src/post_process/script.py
@@ -3,18 +3,23 @@ import matplotlib.pyplot as plt
 import os
 
 def start_sim(routing, dataRate):
-	result = [0]*len(dataRate)
+	result = []
+	dataRate2 = []
 	print routing
 	for n in range(len(dataRate)):
 		print "Inj. Rate:", dataRate[n]
 		with open(os.devnull, "w") as fnull:
 			call(["../booksim", "../examples/chaoconfig", "injection_rate=" + str(dataRate[n]), 
 	          "routing_function = " + routing], stdout=fnull)
-		allData = map(float, open('dump.txt').readlines())
+		try:
+			allData = map(float, open('dump.txt').readlines())
+		except:
+			break
 		os.remove('dump.txt')
-		result[n] = allData[0]
+		result.append(allData[0])
+		dataRate2.append(dataRate[n])
 
-	return result
+	return result, dataRate2
 
 def print_msg(data, routing):
 	print  "======", routing, "latencies======"
@@ -23,26 +28,15 @@ def print_msg(data, routing):
 
 # injection rate
 dataRate = [];
-for n in range(1, 9):
+for n in range(1, 20):
 	dataRate.append(n*0.001)
 
 # start simulation
-# XY routing
-dataXY = start_sim('my_xy', dataRate)
-
-# West first routing
-dataWest = start_sim('west_first', dataRate)
-
-# Negative first routing
-dataNegative = start_sim('negative_first', dataRate)
-
-## Odd Even routing
-dataOddEven = start_sim('odd_even', dataRate)
-
-# NoP routing
-dataNoP = start_sim('my_nop', dataRate)
-
-
+dataXY, dataRateXY = start_sim('my_xy', dataRate)
+dataWest, dataRateWest = start_sim('west_first', dataRate)
+dataNegative, dataRateNegative = start_sim('negative_first', dataRate)
+dataOddEven, dataRateOddEven = start_sim('odd_even', dataRate)
+dataNoP, dataRateNoP = start_sim('my_nop', dataRate)
 
 # print	
 print_msg(dataXY, "XY")
@@ -56,11 +50,11 @@ print_msg(dataNoP, "NoP")
 # plot figure
 fig = plt.figure()
 
-plt.plot(dataRate, dataXY, label='XY')
-plt.plot(dataRate, dataWest, label='West first')
-plt.plot(dataRate, dataNegative, label='Negative first')
-plt.plot(dataRate, dataOddEven, label='Odd Even')
-plt.plot(dataRate, dataNoP, label='NoP')
+plt.plot(dataRateXY, dataXY, label='XY')
+plt.plot(dataRateWest, dataWest, label='West first')
+plt.plot(dataRateNegative, dataNegative, label='Negative first')
+plt.plot(dataRateOddEven, dataOddEven, label='Odd Even')
+plt.plot(dataRateNoP, dataNoP, label='NoP')
 
 plt.legend(loc='upper left')
 plt.xlabel('Injection Rate (Packets/Cycle)')
